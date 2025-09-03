16159dd99c0cf76330b2f6c3c7ba36c1c98b5f09
Beatriz Lopes <95252572+lopsbea@users.noreply.github.com>
2025-09-03 00:24:07 +0000

    grid items

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,89 +1,48 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start border border-2 border-indigo-600">
-        <section className='bg-[#fff]'>
-          <div>lopsbea</div>
-          <div>marcusvrpereira</div>          
-          <div>mavi10          
-          <div>menottelas10</div>
-</div>
-        </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+  const baraoLogo = {borderRadius: '50%', margin: 'auto'}
+
+  const gridItemStyle= 'border-2 border-solid border-white p-4 rounded-xl'
+
+  return (
+    <div className="font-sans items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20">
+     <span className='w-[200px] h-[200px] overflow-hidden'>
+      
+     <Image alt='' src='/barao.jpg' width={200} height={200} style={baraoLogo} /> 
+     </span>
+        <ul className='grid grid-cols-2 w-100% gap-4'>
+          <ul className={gridItemStyle} >
+            @lopsbea
+            <p>bullet:</p>            
+            <p>rapid:</p>
+            <p>blitz:</p>
+            <p>puzzle:</p>
+          </ul>
+          <ul className={gridItemStyle} >
+            <p></p>@marcusvrpereira
+            <p>bullet:</p>            
+            <p>rapid:</p>
+            <p>blitz:</p>
+            <p>puzzle:</p>
+          </ul>          
+          <ul className={gridItemStyle} >@mavi10
+            <p>bullet:</p>            
+            <p>rapid:</p>
+            <p>blitz:</p>
+            <p>puzzle:</p>
+          </ul>
+        </ul>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+         Feito por Bia - T33 :)
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+  
+       
       </footer>
     </div>
   );
